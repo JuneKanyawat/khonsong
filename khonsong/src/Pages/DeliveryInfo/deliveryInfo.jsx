82cfcfc35fd421e9e331d7de0d1ef3d76b6e674b
Kanyawat Raksawin <kanyawat2002@gmail.com>
2024-03-28 19:07:16 +0700

    deli

--- khonsong/src/Pages/DeliveryInfo/deliveryInfo.jsx
+++ khonsong/src/Pages/DeliveryInfo/deliveryInfo.jsx
@@ -1,19 +1,17 @@
 import "./deliveryInfo.css";
 
 const DeliveryInfo = () => {
+  return (
+    <div>
+      <div className="delivery-container">
+        <h1> Status </h1>
+      </div>
 
-    return (
-        <div>
-            <div className = "container">
-                <h1> Status </h1>
-            </div>
-
-            <div className = "container" style={{ marginTop: '20px'}}>
-                <h1> All Status </h1>
-            </div>
-        </div>
-    )
-
+      <div className="container" style={{ marginTop: "20px" }}>
+        <h1> All Status </h1>
+      </div>
+    </div>
+  );
 };
 
 export default DeliveryInfo;
