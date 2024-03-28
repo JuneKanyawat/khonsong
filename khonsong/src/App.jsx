a32fb6550fedd4214c81d52203db9924b168f4bc
Pannmekmok <pannmekmok@gmail.com>
2024-03-28 14:02:20 +0700

    delivery status

--- khonsong/src/App.jsx
+++ khonsong/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import InputOrder from "../src/Pages/InputOrder/inputOrder.jsx";
 import ShowHistory from "../src/Pages/ShowHistory/showHistory.jsx";
+import DeliveryInfo from "./Pages/DeliveryInfo/deliveryInfo.jsx";
 import "./App.css";
 
 function App() {
   return (
     <>
       {/* <InputOrder /> */}
-      <ShowHistory />
+      {/* <ShowHistory /> */}
+      <DeliveryInfo />
     </>
   );
 }
---
+++ khonsong/src/Pages/DeliveryInfo/deliveryInfo.jsx
@@ -0,0 +1,19 @@
+import "./deliveryInfo.css";
+
+const DeliveryInfo = () => {
+
+    return (
+        <div>
+            <div className = "container">
+                <h1> Status </h1>
+            </div>
+
+            <div className = "container" style={{ marginTop: '20px'}}>
+                <h1> All Status </h1>
+            </div>
+        </div>
+    )
+
+};
+
+export default DeliveryInfo;
