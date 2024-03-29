dea58b20b107b1e627e8450e66ab5ba9dabe010a
Pannmekmok <pannmekmok@gmail.com>
2024-03-29 15:05:27 +0700

    delivery page update

--- khonsong/src/Pages/DeliveryInfo/deliveryInfo.jsx
+++ khonsong/src/Pages/DeliveryInfo/deliveryInfo.jsx
@@ -1,14 +1,39 @@
 import "./deliveryInfo.css";
+import Datatable from 'react-data-table-component'
 
 const DeliveryInfo = () => {
+  const categories = [
+    {name: 'Checkpoint', selector: row => row.point}, 
+    {name: 'Receiver ID', selector: row => row.id}, 
+    {name: 'Arrival Time', selector: row => row.arr}, 
+    {name: 'Received Time', selector: row => row.rec}, 
+    {name: 'Order Status', selector: row => row.status},
+    {name: 'Image', selector: row => row.img} 
+  ];
+
+  const data = [
+    {
+      point: 'A',
+      id: 1,
+      arr: '10:00:00',
+      rec: '10:01:00',
+      status: 'completed',
+      img: '-',
+    }
+  ]
+
   return (
     <div>
-      <div className="delivery-container">
+      <div className = "container1">
         <h1> Status </h1>
       </div>
 
-      <div className="container" style={{ marginTop: "20px" }}>
+      <div className = "container2">
         <h1> All Status </h1>
+        <Datatable
+          columns = {categories}
+          data = {data}
+        ></Datatable>
       </div>
     </div>
   );
